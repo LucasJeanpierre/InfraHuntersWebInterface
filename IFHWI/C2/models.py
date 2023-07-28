d27e4ecd87d3cf39df8595562b03c5484dd57fef
LucasJeanpierre <61747163+LucasJeanpierre@users.noreply.github.com>
2023-07-28 10:39:25 +0200

    Agent model

--- IFHWI/C2/models.py
+++ IFHWI/C2/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Agent(models.Model):
+    name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    last_online = models.DateTimeField(auto_now=True)
+    host = models.CharField(max_length=50)  
+    ip = models.CharField(max_length=50)
+    port = models.IntegerField()
+    os = models.CharField(max_length=50)
+    os_version = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    commands = models.ManyToManyField('Command', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Command(models.Model):
+    name = models.CharField(max_length=50)
+    syntax = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
--- IFHWI/C2/views.py
+++ IFHWI/C2/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Agent, Command
+
 
 # Create your views here.
+
+def index(request):
+    agents = Agent.objects.all()
+
+    for agent in agents:
+        agent.command_list = agent.commands.all()
+
+    context = {
+        'agents': agents,
+    }
+
+    return render(request, context=context, template_name='C2/index.html')
